c912b86ae04e65ebaaa63e68b796423d77cb0dfd
ToonVish <eric.stampa@t-online.de>
2024-12-01 09:32:19 +0100

    first commit

---
+++ day01/src/main.rs
@@ -0,0 +1,50 @@
+use std::fs;
+
+fn main() {
+    let lines = read_file("input");
+    let mut list_a = vec![];
+    let mut list_b = vec![];
+
+    for line in lines {
+        let numbers = line
+            .split(" ")
+            .filter(|x| !x.trim().is_empty())
+            .map(|x| x.trim().parse::<i64>().unwrap())
+            .collect::<Vec<i64>>();
+        if numbers.len() == 2 {
+            list_a.push(numbers[0]);
+            list_b.push(numbers[1]);
+        }
+    }
+
+    list_a.sort();
+    list_b.sort();
+
+    //task 1
+    let mut result = 0;
+
+    for i in 0..list_a.len() {
+        result += (list_a[i] - list_b[i]).abs();
+    }
+    println!("Task1: {}", result);
+
+    //task2
+    let mut result = 0;
+
+    for value in list_a {
+        result += value
+            * list_b
+                .clone()
+                .into_iter()
+                .filter(|x| *x == value)
+                .collect::<Vec<i64>>()
+                .len() as i64;
+    }
+
+    println!("Task2: {}", result);
+}
+
+fn read_file(path: &str) -> Vec<String> {
+    let content = fs::read_to_string(path).expect("Failed to read file");
+    content.split("\n").map(|x| x.to_owned()).collect()
+}
