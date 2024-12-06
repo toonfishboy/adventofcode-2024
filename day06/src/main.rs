9b9fb8da8a11b8c0eb932a29c94630bdcd27eff1
Eric Stampa <eric.stampa@cgi.com>
2024-12-06 12:07:39 +0100

    chore: improve day06

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,4 +1,3 @@
-use core::panic;
 use std::{collections::HashSet, fs};
 
 fn find_start(lines: &[Vec<char>]) -> [i64; 2] {
@@ -17,26 +16,18 @@ fn find_start(lines: &[Vec<char>]) -> [i64; 2] {
     pos
 }
 
-fn rotate(dir: &[i64; 2]) -> [i64; 2] {
-    match dir {
-        [-1, 0] => [0, 1],
-        [0, 1] => [1, 0],
-        [1, 0] => [0, -1],
-        [0, -1] => [-1, 0],
-        _ => panic!("This dir should nerver occure"),
-    }
-}
+static DIRS: &[[i64; 2]; 4] = &[[-1, 0], [0, 1], [1, 0], [0, -1]];
 
 fn main() {
-    let lines = read_file("input");
-    let mut dir: [i64; 2] = [-1, 0];
+    let mut lines = read_file("input");
+    let mut dir = 0;
     let start_pos = find_start(&lines);
     let mut pos = start_pos;
 
     let mut positions: HashSet<[i64; 2]> = HashSet::new();
     loop {
         positions.insert(pos);
-        let next_pos = [pos[0] + dir[0], pos[1] + dir[1]];
+        let next_pos = [pos[0] + DIRS[dir][0], pos[1] + DIRS[dir][1]];
         if next_pos[0] < 0
             || next_pos[0] >= lines.len() as i64
             || next_pos[1] < 0
@@ -47,20 +38,23 @@ fn main() {
 
         let next_char = lines[next_pos[0] as usize][next_pos[1] as usize];
         if next_char == '#' {
-            dir = rotate(&dir);
+            dir += 1;
+            dir %= 4;
         } else {
-            pos = [pos[0] + dir[0], pos[1] + dir[1]];
+            pos = [pos[0] + DIRS[dir][0], pos[1] + DIRS[dir][1]];
         }
     }
 
     println!("Task1: {}", positions.len());
+
     let mut result = 0;
-    for (i, line) in lines.iter().enumerate() {
-        for j in 0..line.len() {
+    for (i, line) in lines.clone().iter().enumerate() {
+        for (j, c) in line.iter().enumerate() {
             let map_pos = [i as i64, j as i64];
             if positions.contains(&map_pos)
-                && map_loops(lines.clone(), start_pos, map_pos)
+                && map_loops(&mut lines, start_pos, map_pos)
                 && map_pos != start_pos
+                && *c != '#'
             {
                 result += 1;
             }
@@ -70,36 +64,42 @@ fn main() {
     println!("Task2: {}", result);
 }
 
-fn map_loops(lines: Vec<Vec<char>>, start_pos: [i64; 2], block_pos: [i64; 2]) -> bool {
-    let mut lines = lines.clone();
+fn map_loops(lines: &mut [Vec<char>], start_pos: [i64; 2], block_pos: [i64; 2]) -> bool {
+    let prev_value = lines[block_pos[0] as usize][block_pos[1] as usize];
     lines[block_pos[0] as usize][block_pos[1] as usize] = '#';
 
-    let mut dir: [i64; 2] = [-1, 0];
+    let mut dir = 0;
     let mut pos = start_pos;
     let mut visited: HashSet<[i64; 4]> = HashSet::new();
 
+    let mut is_loop = false;
     loop {
-        let state = [pos[0], pos[1], dir[0], dir[1]];
+        let state = [pos[0], pos[1], DIRS[dir][0], DIRS[dir][1]];
         if visited.contains(&state) {
-            return true;
+            is_loop = true;
+            break;
         }
         visited.insert(state);
-        let next_pos = [pos[0] + dir[0], pos[1] + dir[1]];
+        let next_pos = [pos[0] + DIRS[dir][0], pos[1] + DIRS[dir][1]];
         if next_pos[0] < 0
             || next_pos[0] >= lines.len() as i64
             || next_pos[1] < 0
             || next_pos[1] >= lines[0].len() as i64
         {
-            return false;
+            break;
         }
 
         let next_char = lines[next_pos[0] as usize][next_pos[1] as usize];
         if next_char == '#' {
-            dir = rotate(&dir);
+            dir += 1;
+            dir %= 4;
         } else {
-            pos = [pos[0] + dir[0], pos[1] + dir[1]];
+            pos = [pos[0] + DIRS[dir][0], pos[1] + DIRS[dir][1]];
         }
     }
+
+    lines[block_pos[0] as usize][block_pos[1] as usize] = prev_value;
+    is_loop
 }
 
 fn read_file(path: &str) -> Vec<Vec<char>> {
